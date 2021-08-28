91c97f136aea5e495e053e92b455d6a2091bf24b
juniorpeves <juniorpeves@gmail.com>
2021-08-27 19:43:33 -0500

    clase-16-add_remove

--- add_remove_elements.py
+++ add_remove_elements.py
@@ -10,7 +10,30 @@ class AddRemoveElements(unittest.TestCase):
         driver.maximize_window()
         driver.get("http://the-internet.herokuapp.com/")
         driver.find_element_by_link_text('Add/Remove Elements').click()
-
+    
+    def test_add_remove(self):
+        driver = self.driver
+        elements_added = int(input('How many element will you add?')) #Input cuantos agregar
+        elements_removed = int(input('How many elements will you remove?')) #Input cuantos eliminar
+        total_elements = elements_added - elements_removed
+        
+        add_buttonn = driver.find_element_by_xpath('/html/body/div[2]/div/div/button') #Seleccionando el botón add
+        sleep(3)
+        
+        for i in range(elements_added):
+            add_buttonn.click() #agregar elemento    
+        for i in range(elements_removed):
+            try: #Try para eliminar elemento
+                delete_button = driver.find_element_by_xpath('/html/body/div[2]/div/div/div/button[1]')
+                delete_button.click()
+            except: #Except para no eliminar mas de los existentes
+                print("You're trying to delete more elements the existent")                
+        if total_elements > 0: #Condicional para mostrar cuantos elementos quedan
+            print(f"There are {total_elements} elements on screen")
+        else:
+            print("There 0 are elements on screen")
+        
+        sleep(3) #Pausa
             
     def tearDown(self):
         self.driver.implicitly_wait(5)
